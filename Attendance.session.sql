b546c7d6e4277b54c3c8faad5b462a5ba3858e20
Sha1kh4 <adnan.shaikh66@mit.asia>
2023-09-10 13:38:33 +0530

    added another table to save experience

--- Attendance.session.sql
+++ Attendance.session.sql
@@ -1,6 +1,6 @@
 -- @block
 CREATE TABLE `info`(
-  `no` int(11) NOT NULL,
+  `no` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) DEFAULT NULL,
   `img` varchar(255) DEFAULT NULL,
   `position` varchar(55) DEFAULT NULL,
@@ -17,13 +17,44 @@ CREATE TABLE `info`(
   `insta` varchar(55) DEFAULT NULL,
   `twt` varchar(55) DEFAULT NULL,
   `exp` int(11) DEFAULT NULL,
-  `exp1name` varchar(95) DEFAULT NULL,
-  `exp1place` varchar(55) DEFAULT NULL,
-  `exp1time` varchar(25) DEFAULT NULL,
-  `exp1info` varchar(255) DEFAULT NULL
+  PRIMARY KEY (no)
 );
 
 -- @block
 
- INSERT INTO `info` (`no`, `img`, `position`, `contact`, `resume`, `skill1`, `skill2`, `skill3`, `skill4`, `skill5`, `about`, `linkedin`, `exp`, `exp1name`, `exp1place`, `exp1time`, `exp1info`, `name`, `insta`, `twt`, `fb`) VALUES
-(1, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_AlubbpWqdGsd8Fcqi4fp-E9kCFwD0eM1rdu1NeXTVe3q9R07', 'Professor ', 'mailto:malan@harvard.edu', 'https://cs.harvard.edu/malan/cv/malan.pdf', 'C++', 'GO', 'JAVA', 'PHP', 'Python', 'David J. Malan is Gordon McKay Professor of the Practice of Computer Science at Harvard University in the School of Engineering and Applied Sciences as well as a Member of the Faculty of Education in the Graduate School of Education. He teaches Computer Science 50, otherwise known as CS50, which is among Harvard University’s largest courses, one of Yale University’s largest courses, and edX’s largest MOOC, with over 5.1M registrants. He also teaches at Harvard Business School, Harvard Law School, Harvard Extension School, and Harvard Summer School. ', 'https://www.linkedin.com/in/malan/', 3, 'Lecturer', 'Tufts University', '2002-2005', 'Instructor for Computer Science 15: Data Structures, a second course for undergraduates. Managed staff of 10 teaching assistants', 'David J. Malan', 'https://www.instagram.com/davidjmalan/', 'https://twitter.com/davidjmalan', 'https://www.facebook.com/dmalan');
+ INSERT INTO `info` (`no`, `img`, `position`, `contact`, `resume`, `skill1`, `skill2`, `skill3`, `skill4`, `skill5`, `about`, `linkedin`, `exp`, `name`, `insta`, `twt`, `fb`) VALUES
+(1, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_AlubbpWqdGsd8Fcqi4fp-E9kCFwD0eM1rdu1NeXTVe3q9R07', 'Professor ', 'mailto:malan@harvard.edu', 'https://cs.harvard.edu/malan/cv/malan.pdf', 'C++', 'GO', 'JAVA', 'PHP', 'Python', 'David J. Malan is Gordon McKay Professor of the Practice of Computer Science at Harvard University in the School of Engineering and Applied Sciences as well as a Member of the Faculty of Education in the Graduate School of Education. He teaches Computer Science 50, otherwise known as CS50, which is among Harvard University’s largest courses, one of Yale University’s largest courses, and edX’s largest MOOC, with over 5.1M registrants. He also teaches at Harvard Business School, Harvard Law School, Harvard Extension School, and Harvard Summer School. ', 'https://www.linkedin.com/in/malan/', 3, 'David J. Malan', 'https://www.instagram.com/davidjmalan/', 'https://twitter.com/davidjmalan', 'https://www.facebook.com/dmalan');
+
+-- @block
+CREATE TABLE experience(`id` int(11) NOT NULL AUTO_INCREMENT,
+  `exp1name` varchar(95) DEFAULT NULL,
+  `exp1place` varchar(55) DEFAULT NULL,
+  `exp1time` varchar(25) DEFAULT NULL,
+  `exp1info` varchar(255) DEFAULT NULL,
+  `exp2name` varchar(95) DEFAULT NULL,
+  `exp2place` varchar(55) DEFAULT NULL,
+  `exp2time` varchar(25) DEFAULT NULL,
+  `exp2info` varchar(255) DEFAULT NULL,
+  `exp3name` varchar(95) DEFAULT NULL,
+  `exp3place` varchar(55) DEFAULT NULL,
+  `exp3time` varchar(25) DEFAULT NULL,
+  `exp3info` varchar(255) DEFAULT NULL,
+  `exp4name` varchar(95) DEFAULT NULL,
+  `exp4place` varchar(55) DEFAULT NULL,
+  `exp4time` varchar(25) DEFAULT NULL,
+  `exp4info` varchar(255) DEFAULT NULL,  
+  PRIMARY KEY (id),`no` int(11),
+  FOREIGN KEY (no) REFERENCES info(no)
+);
+
+-- @block
+INSERT INTO experience(`exp1name`, `exp1place`, `exp1time`, `exp1info`) VALUES( 'Lecturer', 'Tufts University', '2002-2005', 'Instructor for Computer Science 15: Data Structures, a second course for undergraduates. Managed staff of 10 teaching assistants');
+
+-- @block
+UPDATE  experience SET exp2name='Founder', exp2place= 'Diskaster', exp2time='2005 – 2008', exp2info='Started company that offered professional recovery of data from hard drives and memory cards as
+well as forensic investigations for civil matters.' where id = 1;
+
+
+-- @block
+SELECT * FROM info;
+SELECT * FROM experience;
--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask, session, request, g, render_template, redirect, url_for,flash
 from flask_sqlalchemy import SQLAlchemy
+from flask_mysqldb import MySQL
+import mysql.connector as sql
+
+import os
 
 app = Flask(__name__)
 app.secret_key = "1234"
@@ -11,7 +15,7 @@ db = SQLAlchemy(app)
 class info(db.Model):
     no = db.Column(db.Integer, primary_key=True)
     img = db.Column(db.String(255))
-    Name = db.Column(db.String(255))
+    name = db.Column(db.String(255))
     position = db.Column(db.String(255))
     contact = db.Column(db.String(255))
     resume = db.Column(db.String(255))
@@ -25,8 +29,65 @@ class info(db.Model):
     fb = db.Column(db.String(255))
     insta = db.Column(db.String(255))
     twt = db.Column(db.String(255))
-    exp = db.Column(db.Integer(2))
-    exp1name = db.Column(db.String(255))
-    exp1place = db.Column(db.String(255))
+    exp = db.Column(db.Integer)
+
+
+class experience(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    exp1name = db.Column(db.String(255)) 
+    exp1place  = db.Column(db.String(255))
     exp1time = db.Column(db.String(255))
-    exp1info = db.Column(db.Text)
+    exp1info = db.Column(db.String(255))
+    exp2name = db.Column(db.String(255))
+    exp2place = db.Column(db.String(255))
+    exp2time = db.Column(db.String(255))
+    exp2info = db.Column(db.String(255))
+    exp3name = db.Column(db.String(255))
+    exp3place = db.Column(db.String(255))
+    exp3time = db.Column(db.String(255))
+    exp3info = db.Column(db.String(255))
+    exp4name = db.Column(db.String(255))
+    exp4place = db.Column(db.String(255))
+    exp4time = db.Column(db.String(255))
+    exp4info = db.Column(db.String(255))
+    no = db.Column(db.Integer, db.ForeignKey("info.no"))
+
+@app.route('/')
+def main():
+    return "hello"
+
+
+@app.route('/teach')
+def teach():
+
+    infos = info.query.filter_by(no=1).first()
+    exp = experience.query.filter_by(id=1).first()
+    print(exp.exp1name)
+    return render_template("teach.html", db=infos,exp=exp)
+
+
+
+# @app.route('/edit',methods=['GET', 'POST'])
+# def edit():
+#         if(request.method=='POST'):
+#             name  = request.form.get('name')
+#             position = request.form.get('position')
+#             contact = request.form.get('contact')
+#             resume = request.form.get('resume')
+#             skill1 = request.form.get('skill1')
+#             skill2 = request.form.get('skill2')
+#             skill3 = request.form.get('skill3')
+#             skill4 = request.form.get('skill4')
+#             skill5 = request.form.get('skill5')
+#             # info = request.form.get('info')
+#             linkedin = request.form.get('linkedin')
+#             insta = request.form.get('insta')
+#             twt = request.form.get('twt')
+#             exp = request.form.get('exp')
+            
+
+#             entry=info(name=name,position=position,contact=contact,resume=resume,skill1=skill1,skill2=skill2,skill3=skill3,skill4=skill4,skill5=skill5,linkedin=linkedin,insta=insta,twt=twt,exp=exp)
+#             db.session.add(entry)
+#             db.session.commit()
+
+#         return render_template('form.html')
